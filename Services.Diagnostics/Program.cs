70417447cc5eb2e6b6e660fde7eaff6ab6500d5e
Mayur Sapaliga <msapaliga@collabware.com>
2023-02-03 11:58:20 -0800

    Add project files.

---
+++ Services.Diagnostics/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace Services.Diagnostics
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+
+        public string PrintDiagnostics()
+        {
+            string message = "Hello from diagnostics service";
+            Console.WriteLine(message);
+            return message;
+        }
+    }
+}
